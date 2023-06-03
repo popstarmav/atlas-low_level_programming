a6e3fe1686d45352a387336da68e5689674dbd15
popstarmav <cj.johnson2017@gmail.com>
2023-06-03 16:30:26 -0700

    script

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Prints "_putchar" followed by a new line.
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
+
