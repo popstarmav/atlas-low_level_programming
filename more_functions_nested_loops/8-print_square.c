beb634ebe812611e6a44d7fba070684a843d0741
popstarmav <cj.johnson2017@gmail.com>
2023-06-05 12:36:48 -0700

    script

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,17 +9,18 @@ void print_square(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		for (j = 0; j < size; j++)
+		int i, j;
+
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
