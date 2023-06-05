fc48f63cda95b50a6b279b4df8e63e6bacaa6b0f
popstarmav <cj.johnson2017@gmail.com>
2023-06-04 21:18:08 -0700

    script

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 int
+ * @a: First int
+ * @b: Second int
+ * Return: additon of the multiplicattion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
