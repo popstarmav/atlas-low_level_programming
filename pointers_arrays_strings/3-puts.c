60d2530446d6d2adc9f18ef51fb437a35cae553d
popstarmav <cj.johnson2017@gmail.com>
2023-06-09 11:19:48 -0700

    script

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -4,8 +4,9 @@
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  */
-void _puts(char *str);
 {
+
+void _puts(char *str);
 	int (i);
 
 	for (i = 0; str[i] != '\0'; i++)
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,3 +19,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
+
+char *strcat(char *dest, char *src);
