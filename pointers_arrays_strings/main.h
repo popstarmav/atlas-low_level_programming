0ba355d59ff4ff7ce1824f596c6524abe4d4d692
popstarmav <cj.johnson2017@gmail.com>
2023-06-11 05:36:47 -0700

    script

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,4 +20,6 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
-char *strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
