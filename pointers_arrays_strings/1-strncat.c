2d963947af408db4f5f3598caf794236bb01659d
popstarmav <cj.johnson2017@gmail.com>
2023-06-11 07:21:29 -0700

    UPDATE

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,29 +2,30 @@
 #include <string.h>
 
 /**
- * _strnact - Concatenates two strings
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
- * Return: A pointer to the resulting string dest.
+ * _strnact - Concatenate strings, defining the size of sencd string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
  */
+
 char *_strnact(char *dest, char *src, int n)
 {
-	int i;
-	int c;
+
+	int i, c;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	c = 0;
-	while (c < n && src[c] != '\0')
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[c] != '\0' && i < 97 && c < n)
 	{
-	dest[i] = src[c];
-	i++;
-	c++;
+		(dest[i]) = (src[c]);
+		i++;
+		c++;
 	}
-	dest[i] = '\0';
+	(dest[c] = '\0');
 	return (dest);
 }
