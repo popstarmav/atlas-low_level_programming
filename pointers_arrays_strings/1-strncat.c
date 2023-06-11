3b2a9a82c201f8fdad5dad6501719efc93012713
popstarmav <cj.johnson2017@gmail.com>
2023-06-11 07:48:07 -0700

    UPDATE

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,21 @@
 char *_strnact(char *dest, char *src, int n)
 {
 
-	int i, c;
+	int i;
+	int c;
 
 	i = 0;
-	c = 0;
-
 	while (dest[i] != '\0')
+	{
 		i++;
-
-	while (src[c] != '\0' && i < 97 && c < n)
+	}
+	c = 0;
+	while (c < n && src[c] != '\0')
 	{
 		(dest[i]) = (src[c]);
 		i++;
 		c++;
 	}
-	(dest[c] = '\0');
+	dest[i] = '\0';
 	return (dest);
 }
