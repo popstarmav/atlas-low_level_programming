da163440b6e76717457b28f6177c64f8e230e51c
popstarmav <cj.johnson2017@gmail.com>
2023-06-14 23:30:42 -0700

    UPDATE

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: pinter to the string
+ * _print_rev_recursion - Prints a string in reverse, followed by a new line.
+ * @s: the string to be printed
  *
- * Return: void
  */
-
-void _print_rev_reverse(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (s[0] != '\0')
 	{
-		_print_rev_reverse(s + 1);
-		putchar(*s);
+		_print_rev_recursion(s + 1);
+		putchar(s[0]);
 	}
 }
