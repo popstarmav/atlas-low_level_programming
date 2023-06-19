ea8fa15b671c7bdc8e5c244568cad9718007bfc9
popstarmav <cj.johnson2017@gmail.com>
2023-06-19 01:19:31 -0700

    UPDATE

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -7,33 +7,11 @@
  *@argv: char.
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int k;
-	int mult = 0;
+	(void)argv;
+	printf("%d\n", argc - 1);
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (0);
-	}
-	else if (argc == 3)
-	{
-		for (i = 0 ; i < argc ; i++)
-		{
-			if (i == 1)
-			{
-				j = atoi(argv[i]);
-			}
-			else if (i == 2)
-			{
-				k = atoi(argv[i]);
-			}
-		}
-	mult = j * k;
-	printf("%d\n", mult);
-	}
 	return (0);
 }
