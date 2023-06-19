8ab90a650b4062a6e888a0dc0fd530af9dc45d48
popstarmav <cj.johnson2017@gmail.com>
2023-06-19 01:26:05 -0700

    UPDATE

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -10,8 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	(void)argv;
-	printf("%d\n", argc - 1);
+	printf("%s\n", argv[0]);
+	for (int i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }
