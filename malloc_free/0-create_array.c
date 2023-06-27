4102643d5184cb58dc23beedca554507e6f8aa7f
popstarmav <cj.johnson2017@gmail.com>
2023-06-26 22:36:15 -0700

    script

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,43 +10,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *s;
+
 	if (size == 0)
 		return (NULL);
 
-	char *s = malloc((size + 1) * sizeof(char));
+	s = malloc((size + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
 
 	for (unsigned int i = 0; i < size; i++)
+	{
 		s[i] = c;
-
-	s[size] = '\0';
+	}
 
 	return (s);
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: ALways 0.
- */
-int main(void)
-{
-	unsigned int size = 5;
-	char c = 'A';
-
-	char *my_array = create_array(size, c);
-
-	if (my_array != NULL)
-	{
-		printf("Array: %s\n", my_array);
-		free(my_array);
-	}
-	else
-	{
-		printf("Failed to create the array. \n");
-	}
-
-	return (0);
 }
