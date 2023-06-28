e95801e8654e042ff43b12909dbb2445255f8c71
popstarmav <cj.johnson2017@gmail.com>
2023-06-27 17:58:10 -0700

    UPDATE

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Duplicate a string
+ * @str: The string to be duplicated
+ *
+ * Return: NULL
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	size_t (length) = strlen(str);
+
+	char *duplicate = (char *)malloc((length + 1) * sizeof(char));
+
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy (duplicate, str);
+
+	return (duplicate);
+
+}
