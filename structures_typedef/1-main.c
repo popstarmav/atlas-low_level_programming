13df87d2f0a6e99f3740154dd035c08ad6164171
popstarmav <cj.johnson2017@gmail.com>
2023-07-03 07:19:54 -0700

    UPDATE

--- structures_typedef/1-main.c
+++ structures_typedef/1-main.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-    struct dog my_dog;
+	struct dog my_dog;
 
-    int_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	int_dog(&my_dog, "Poppy", 3.5, "Bob");
+
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
 }
