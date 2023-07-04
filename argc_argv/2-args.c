c3cc3f2a3df5f9ebe91ec12e046798e728120ad3
popstarmav <cj.johnson2017@gmail.com>
2023-07-03 20:51:25 -0700

    UPDATE

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /**
- * main -  print name
- *@argc: int.
- *@argv: char.
+ * main -  print all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
  * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	for (int i = 1; i < argc; i++)
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
