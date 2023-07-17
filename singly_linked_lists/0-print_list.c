b68f0b6f1f3f2076b31347d8543e62f57796df97
popstarmav <cj.johnson2017@gmail.com>
2023-07-17 04:16:28 -0700

    UPDATE

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lists.h"
+#include "main.h"
 
 /**
  * print_list - prints all the elements of a linked list
@@ -23,4 +23,3 @@ size_t print_list(const list_t *h)
 
 	return (s);
 }
-
