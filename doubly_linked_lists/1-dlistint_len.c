ce1d5b822ea4553eabc9c3d68329039204960baf
popstarmav <cj.johnson2017@gmail.com>
2023-07-24 13:41:53 -0700

    Everything is Updated

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,22 +1,8 @@
 #include <stddef.h>
+#include "lists.h"
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * 
- */
-typedef struct dlistint_s
-{
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
-
-/**dlistint_len -returns the number of elements in a double linked list
+ * dlistint_len - returns the number of elements in a double linked list
  * @h: head of the list
  *
  * Return: The number of nodes in the list
@@ -32,5 +18,5 @@ size_t dlistint_len(const dlistint_t *h)
 		current = current->next;
 	}
 
-	return(count);
+	return (count);
 }
