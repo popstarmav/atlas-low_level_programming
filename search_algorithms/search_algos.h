d040f4fcd3d04b99d898e83f29e8ce77352ad2b4
popstarmav <cj.johnson2017@gmail.com>
2023-08-21 22:11:32 -0700

    C - Search Algorithms

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,29 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX Holberton project
+ */
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
