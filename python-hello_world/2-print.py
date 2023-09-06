c12a3528cd714590f5fca58f95f12c0ef8c5f87b
popstarmav <cj.johnson2017@gmail.com>
2023-09-05 17:46:49 -0700

    Print script

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# This is a script that prints a message
+# The print function display the message "Programming is like building a multilingual puzzle"
+
+print("Programming is like building a multilingual puzzle")
