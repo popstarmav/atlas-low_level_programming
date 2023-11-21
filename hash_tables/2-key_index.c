4e6c2c46fed43b25b89fbca7bf5f6f9b2f709aca
popstarmav <cj.johnson2017@gmail.com>
2023-11-20 21:42:15 -0800

    key -> index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -11,18 +11,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
-    unsigned long int i;
-
-    if (key == NULL || size == 0)
-        return (0);
-
-    hash_value = 0;
-
-    for (i = 0; key[i] != '\0'; i++)
-    {
-        hash_value = (hash_value * 31) + key[i];
-    }
-
-    return (hash_value % size);
+    return (hash_djb2(key) % size);
 }
