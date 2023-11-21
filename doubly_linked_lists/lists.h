057d22f9b542a778fd135d4a32cd57214c9beaf2
popstarmav <cj.johnson2017@gmail.com>
2023-11-20 20:15:55 -0800

    doubley link list

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,23 +3,17 @@
 
 #include <stddef.h>
 
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- *
- */
-typedef struct dlistint {
+/* Structure for a doubly linked list node */
+typedef struct dlistint_s
+{
     int n;
-    struct dlistint *prev;
-    struct dlistint *next;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
-/* Declare the print_dlistint function */
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
