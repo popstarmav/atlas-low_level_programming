ae495752ecdfd9f15092ec78466f2e1311cd501b
popstarmav <cj.johnson2017@gmail.com>
2023-11-20 19:21:24 -0800

    List length

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -7,17 +7,17 @@
  * Return: The number of elements in the list.
  */
 
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
 
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
+    	while (h != NULL)
+    	{
+	    count++;
+	    h = h->next;
 	}
-
-
+	
+	
 	return (count);
 }
