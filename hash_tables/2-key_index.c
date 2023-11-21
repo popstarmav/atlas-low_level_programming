c78691ead2fc7d445d365a32a18153ce666319ff
popstarmav <cj.johnson2017@gmail.com>
2023-11-20 21:30:25 -0800

    key -> index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -2,21 +2,27 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Computes the index of a key in the hash table array.
+ * key_index - Calculates the index for a key in the hash table array.
  * @key: The key to be hashed.
  * @size: The size of the hash table array.
  *
- * Return: The index at which the key/value pair should be stored.
+ * Return: Index for storing the key/value pair.
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+    unsigned long int hash_value;
+    unsigned long int i;
+
     if (key == NULL || size == 0)
         return (0);
 
-    unsigned long int hash_value = 0;
+    hash_value = 0;
 
-    for (unsigned long int i = 0; key[i] != '\0'; i++)
+    for (i = 0; key[i] != '\0'; i++)
+    {
         hash_value = (hash_value * 31) + key[i];
+    }
 
     return (hash_value % size);
 }
