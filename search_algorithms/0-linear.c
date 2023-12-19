de3717360908d3438b927f7a1070dd2c77b88c6a
popstarmav <cj.johnson2017@gmail.com>
2023-12-18 21:07:48 -0600

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,18 +12,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-
-    if (array == NULL)
-        return (-1);
-
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
-
-        if (array[i] == value)
-            return (i);
-    }
-
-    return (-1);
+	size_t i;
+	
+	if (array == NULL)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
+		
+		if (array[i] == value)
+			return (i);
+	}
+	
+	return (-1);
 }
