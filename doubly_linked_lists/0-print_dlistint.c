122023ee0feb7fa7fca39b6f4ed1919ad0f323fb
popstarmav <cj.johnson2017@gmail.com>
2024-03-14 02:08:03 +0000

    print elements of dlistint_t

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return count;
+}
