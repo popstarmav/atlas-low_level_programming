ca8d860a33d7935f37129183f92785769483be6f
popstarmav <cj.johnson2017@gmail.com>
2024-03-14 05:06:49 +0000

    print elements of dlistint_t

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,6 +3,17 @@
 #include <stddef.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints the elements of a doubly link list.
+ *
+ * This function prints elements of a doubly linked list to stdout
+ * and returns the count od printed elements.
+ *
+ *@h: Pointer to the head of the doubly linked list
+ *
+ *Return: Total count of printer
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
