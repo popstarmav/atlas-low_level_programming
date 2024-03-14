94072a230a151d7eba31d77299b32c916ee8ed29
popstarmav <cj.johnson2017@gmail.com>
2024-03-14 05:12:35 +0000

    lists file

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,7 +20,7 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-// function prototypes
+/* function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
