258f7ee18129093b98e64747300f5479c3ce2896
popstarmav <cj.johnson2017@gmail.com>
2024-08-27 23:40:29 -0500

    Fix include directive and update Makefile for basic C project

---
+++ makefiles/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "school.h"
+
+int main()
+{
+    print_school_message();
+    return (0);
+}
---
+++ makefiles/school.h
@@ -0,0 +1,6 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
+void print_school_message(void);
+
+#endif /* SCHOOL_H */
