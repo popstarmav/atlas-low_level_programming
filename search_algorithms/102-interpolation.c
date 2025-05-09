87e359c67282cd3001a15d2334bca9aae7ae1261
popstarmav <cj.johnson2017@gmail.com>
2025-05-09 16:24:10 -0500

    Error: Failed to get git output.

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+int interpolation_search(int *array, size_t size, int value)
+{
+    size_t low = 0;
+    size_t high = size - 1;
+    size_t pos;
+
+    if (array == NULL || size == 0)
+        return (-1);
+
+    while (low <= high && value >= array[low] && value <= array[high])
+    {
+        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+        if (array[pos] == value)
+            return (pos);
+
+        if (array[pos] < value)
+            low = pos + 1;
+        else
+            high = pos - 1;
+    }
+
+    if (value >= array[low] && value <= array[high])
+        return (-1);
+
+    pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+    printf("Value checked array[%lu] is out of range\n", pos);
+    
+    return (-1);
+}
+
